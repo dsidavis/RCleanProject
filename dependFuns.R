d915c53f44fa7a24d5bcaa77e80f261fc1e63cae
Duncan Temple Lang <duncan@r-project.org>
2016-11-15 15:22:47 -0800

    making progress. Reorganized our output data structure to 3 columns. Added functions which deal with files

--- dependFuns.R
+++ dependFuns.R
@@ -1,19 +1,33 @@
 getDepends =
 function(file, info = as(readScript(file), "ScriptInfo"))
 {
-  lapply(info, getDepend)
+  tmp = lapply(info, getDepend)
+  i = !sapply(tmp, is.null)
+  if(!any(i))
+      return(data.frame(filename = character(), operation = character(), expressionNum = integer()))
+  
+  ans = as.data.frame(do.call(rbind, tmp[i]), stringsAsFactors = FALSE)
+  ans$expressionNum = which(i)
+  ans
+}
+
+FileFunctionNames =
+function(..., .funs = unlist(list(...)), .exclude = FALSE)
+{
+   if(.exclude)
+      .funs
+   else
+      c("source", "load", "save", "png", "pdf", "read.csv", "read.table", "read.fwf", "file", "gzfile", "write.csv", .funs)
 }
 
 getDepend =
-function(node)
+function(node, fileFunctionNames = FileFunctionNames())
 {
-   if("source" %in% node@functions)
-      return(structure(c(source = node@strings), class = "SourceFileName"))
-   if("load" %in% node@functions)
-      return(structure(c(source = node@strings), class = "LoadFileName"))
-   if("save" %in% node@functions)
-      return(structure(c(source = node@strings), class = "SaveFileName")   )
-
-   NULL
+   if(length(node@strings) && any(fileFunctionNames %in% names(node@functions))) {
+          # what about sep = "\t"
+      c(filename = node@strings, operation = names(node@functions))
+   }
+   else
+     NULL
 }
 
